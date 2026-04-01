9fb814a3b5b094639059943dbf9add1f2fdf4dbf
Ilfat Aliev <ialiev685@gmail.com>
2026-04-01 16:40:19 +0300

    рефактор генерации и проверки токенов

--- src/services/token-service.ts
+++ src/services/token-service.ts
@@ -1,4 +1,9 @@
 import { TokenModel } from "../models";
+import jwt from "jsonwebtoken";
+import type { UserDto } from "../dto/user";
+
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN_SECRET ?? "";
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN_SECRET ?? "";
 
 class TokenService {
   async saveToken(userId: number, refreshToken: string) {
@@ -10,6 +15,31 @@ class TokenService {
     }
     const createdToken = await TokenModel.create({ userId, refreshToken });
     return createdToken.refreshToken;
+  }
+
+  generateToken(userDto: UserDto) {
+    const refreshToken = jwt.sign({ ...userDto }, REFRESH_TOKEN, {
+      expiresIn: "5Min",
+    });
+    const accessToken = jwt.sign({ ...userDto }, ACCESS_TOKEN, {
+      expiresIn: "30Sec",
+    });
+    return { accessToken, refreshToken };
+  }
+
+  verifyAccessToken(token: string) {
+    try {
+      return jwt.verify(token, ACCESS_TOKEN);
+    } catch (_error) {
+      return null;
+    }
+  }
+  verifyRefreshToken(token: string) {
+    try {
+      return jwt.verify(token, ACCESS_TOKEN);
+    } catch (_error) {
+      return null;
+    }
   }
 }
 
