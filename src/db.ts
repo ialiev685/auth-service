46e2be536755959a0e6cb1a88e4a9af1c34738fa
Ilfat Aliev <ialiev685@gmail.com>
2026-04-17 16:01:37 +0300

    добавлена настройка запуска сервера без docker

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,11 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
 export const sequelize = new Sequelize({
-  host: "db",
-  username: process.env.DB_USERNAME ?? "",
-  password: process.env.DB_PASSWORD ?? "",
+  host: process.env.RUNNING_IN_DOCKER === 'true' ? 'db' : 'localhost',
+  username: process.env.DB_USERNAME ?? '',
+  password: process.env.DB_PASSWORD ?? '',
   port: Number(process.env.DB_PORT) || 5432,
-  database: process.env.DB_NAME ?? "",
-  dialect: "postgres",
+  database: process.env.DB_NAME ?? '',
+  dialect: 'postgres',
   logging: false,
 });
