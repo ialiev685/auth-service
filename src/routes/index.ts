f51f9e53e0c289acfd264d8395443e1235ce41a8
Ilfat Aliev <ialiev685@gmail.com>
2026-04-17 15:26:39 +0300

    рефактор

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
-import { Controller } from "../controller";
-import type { FastifyPluginCallback } from "fastify";
-import { authMiddleware } from "../middleware/auth-middleware";
+import { Controller } from '../controller';
+import type { FastifyPluginCallback } from 'fastify';
+import { authMiddleware } from '../middleware/auth-middleware';
 import {
   activateSchema,
   currentUserSchema,
@@ -9,40 +9,21 @@ import {
   refreshSchema,
   registerSchema,
   resetPasswordSchema,
-} from "../schemas";
+} from '../schemas';
 
-export const REDIRECT_PARAM = "redirectUrl" as const;
-export const UUID_PARAM = "uuid" as const;
+export const REDIRECT_PARAM = 'redirectUrl' as const;
+export const UUID_PARAM = 'uuid' as const;
 
 export const routes: FastifyPluginCallback = (instance, _options, done) => {
   const controller = new Controller(instance);
-
-  instance.post("/register", { schema: registerSchema }, controller.register);
-
+  instance.post('/register', { schema: registerSchema }, controller.register);
+  instance.get(`/activate/:${UUID_PARAM}`, { schema: activateSchema }, controller.activate);
+  instance.post('/login', { schema: loginSchema }, controller.login);
+  instance.post('/forgotPassword', { schema: forgotPasswordSchema }, controller.forgotPassword);
+  instance.post('/resetPassword', { schema: resetPasswordSchema }, controller.resetPassword);
+  instance.post('/refresh', { schema: refreshSchema }, controller.refresh);
   instance.get(
-    `/activate/:${UUID_PARAM}`,
-    { schema: activateSchema },
-    controller.activate,
-  );
-
-  instance.post("/login", { schema: loginSchema }, controller.login);
-
-  instance.post(
-    "/forgotPassword",
-    { schema: forgotPasswordSchema },
-    controller.forgotPassword,
-  );
-
-  instance.post(
-    "/resetPassword",
-    { schema: resetPasswordSchema },
-    controller.resetPassword,
-  );
-
-  instance.post("/refresh", { schema: refreshSchema }, controller.refresh);
-
-  instance.get(
-    "/currentUser",
+    '/currentUser',
     { preHandler: authMiddleware(controller), schema: currentUserSchema },
     controller.currentUser,
   );
